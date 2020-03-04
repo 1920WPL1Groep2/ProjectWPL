a6e0f67cdf0155ac23f96c009ac830b5dfefdbac
Leandro <R0797275@student.vives.be>
2020-03-04 14:50:08 +0100

    1ste opdracht daoafgewerkte

--- src/Dao/DaoPlant.java
+++ src/Dao/DaoPlant.java
@@ -2,20 +2,18 @@ package Dao;
 
 import Model.Plant;
 
-import java.security.PublicKey;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 //Auteur Leandro Ostyn
 
-public class DaoPlant
-{
+public class DaoPlant {
     private final Connection dbConnection;
 
     private static final String GETALLPLANTSBYINITIALISE =
             "SELECT * FROM plant ";
-    
+
     private static final String GETPLANTTBYNAAM =
             "SELECT * FROM plant WHERE fgsv LIKE ?";
 
@@ -23,27 +21,25 @@ public class DaoPlant
             "SELECT * FROM plant WHERE type = ?";
 
 
-
-
-
     private PreparedStatement STMTGETPLANTBYNAAM;
     private PreparedStatement STMTGETPLANTTBYTYPE;
-    private PreparedStatement STMTGETALLPLANTSBYINITIALISE;
+
 
     public DaoPlant(Connection dbConnection) throws SQLException {
         this.dbConnection = dbConnection;
 
     }
 
-    public List<Plant> getallplant()throws SQLException{
-        List<Plant> lijstplant= new ArrayList<Plant>();
-        STMTGETALLPLANTSBYINITIALISE=dbConnection.prepareStatement(GETALLPLANTSBYINITIALISE);
-        ResultSet rs = STMTGETPLANTBYNAAM.executeQuery();
-        while (rs.next()){
-            Plant plant= new Plant(rs.getInt("plant_ID"),rs.getString("type"),rs.getString("familie"),rs.getString("geslacht"),rs.getString("soort"), rs.getString("variatie"), rs.getInt("plantdichtheid_min"),rs.getInt("plantdichtheid_min"));
-                lijstplant.add(plant);}
-return lijstplant;
-            }
+    public List<Plant> getallplant() throws SQLException {
+        List<Plant> lijstplant = new ArrayList<Plant>();
+        Statement stmt = dbConnection.createStatement();
+        ResultSet rs = stmt.executeQuery(GETALLPLANTSBYINITIALISE);
+        while (rs.next()) {
+            Plant plant = new Plant(rs.getInt("plant_ID"), rs.getString("type"), rs.getString("familie"), rs.getString("geslacht"), rs.getString("soort"), rs.getString("variatie"), rs.getInt("plantdichtheid_min"), rs.getInt("plantdichtheid_min"));
+            lijstplant.add(plant);
+        }
+        return lijstplant;
+    }
 
 
    /* public Plant getplantnaam(String naam) throws SQLException {
