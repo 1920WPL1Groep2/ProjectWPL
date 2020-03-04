743311eb823e800154331c0de52a3f1a4c1fdfe5
Leandro <R0797275@student.vives.be>
2020-03-04 11:25:48 +0100

    Dao'plant

--- src/Dao/DaoPlant.java
+++ src/Dao/DaoPlant.java
@@ -4,27 +4,55 @@ import Model.Plant;
 
 import java.sql.*;
 
+//Auteur Leandro Ostyn
+
 public class DaoPlant
 {
     private final Connection dbConnection;
     
     private static final String GETPLANTTBYNAAM =
-            "SELECT * FROM plant WHERE type LIKE ?";
+            "SELECT * FROM plant WHERE fgsv LIKE ?";
+
+    private static final String GETPLANTTBYTYPE =
+            "SELECT * FROM plant WHERE type = ?";
+
+
+
 
 
     private PreparedStatement STMTGETPLANTBYNAAM;
-
+    private PreparedStatement STMTGETPLANTTBYTYPE;
 
     public DaoPlant(Connection dbConnection) throws SQLException {
         this.dbConnection = dbConnection;
-        STMTGETPLANTBYNAAM = dbConnection.prepareStatement(GETPLANTTBYNAAM);
+
     }
 
-    public String getplantnaam(String naam) throws SQLException {
-        String niets= "nietgelukt";
+    public Plant getplantnaam(String naam) throws SQLException {
+        STMTGETPLANTBYNAAM = dbConnection.prepareStatement(GETPLANTTBYNAAM);
+        String niets= "niet gelukt";
         STMTGETPLANTBYNAAM.setString(1, "%"+naam+"%");
 
         ResultSet rs = STMTGETPLANTBYNAAM.executeQuery();
+
+        while (rs.next()) {
+            Plant plant = new Plant(rs.getString("type"))
+            int plantid = rs.getInt("plant_ID");
+            String plantnaam = rs.getString("type");
+            String familie = rs.getString("familie");
+            System.out.println(plantid + "\t" + plantnaam +
+                    "\t" + familie);
+              return plantnaam;
+        }
+        return niets;
+
+    }
+    public void getplanttype(String type) throws SQLException {
+        STMTGETPLANTTBYTYPE = dbConnection.prepareStatement(GETPLANTTBYTYPE);
+        String niets= "niet gelukt";
+        STMTGETPLANTTBYTYPE.setString(1, type);
+
+        ResultSet rs = STMTGETPLANTTBYTYPE.executeQuery();
 
         while (rs.next()) {
             int plantid = rs.getInt("plant_ID");
@@ -32,9 +60,10 @@ public class DaoPlant
             String familie = rs.getString("familie");
             System.out.println(plantid + "\t" + plantnaam +
                     "\t" + familie);
-            //  return plantnaam;
+
         }
-        return niets;
+
 
     }
+
 }
