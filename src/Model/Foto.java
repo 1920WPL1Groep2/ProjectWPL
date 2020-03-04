834687ecb62d21908a9bd93e27ca9427a774742c
Ayoub <r0699832@student.vives.be>
2020-03-04 16:13:27 +0100

    Update 2 Constructor foto + Constructor fenotype

--- src/Model/Foto.java
+++ src/Model/Foto.java
@@ -4,6 +4,7 @@ package Model;
 import java.awt.*;
 
 public class Foto extends PlantID {
+    //variabelen
     private int plant_id;
     private int foto_id;
     private String eigenschap;
@@ -14,17 +15,13 @@ public class Foto extends PlantID {
 
     public Foto(int plant_id, int plant_id1, int foto_id, String eigenschap, String url, Image foto) {
         super(plant_id);
-        this.plant_id = plant_id1;
         this.foto_id = foto_id;
         this.eigenschap = eigenschap;
         this.url = url;
         this.foto = foto;
     }
 
-    public int getPlant_id() {
-        return plant_id;
-    }
-
+    //getters & setters
     public int getFoto_id() {
         return foto_id;
     }
