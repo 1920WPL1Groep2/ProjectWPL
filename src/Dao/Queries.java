1814737be9695d31364a6b14445631739d7f8ee5
Leandro_ostyn <R0797275@student.vives.be>
2020-03-11 09:34:27 +0100

    Volledige afwerking Daozoekscherm + aantal functies in controller geschreven

---
+++ src/Dao/Queries.java
@@ -0,0 +1,48 @@
+package Dao;
+
+import java.sql.PreparedStatement;
+
+public interface Queries {
+
+    // Alles om Daozoekscherm op te vullen
+    public static final String GETALLFAMILIEBYINITIALISE =
+            "SELECT DISTINCT * FROM familie";
+    //Voor combobox Type
+    static final String GETALLTYPEBYINITIALISE =
+            "SELECT DISTINCT * FROM type";
+
+    //Voor Combobox Geslacht
+     static final String GETALLGESLACHTBYINITIALISE =
+            "SELECT DISTINCT * FROM geslacht";
+
+    //Voor Combobox Soort
+     static final String GETALLSOORTBYINITIALISE =
+            "SELECT DISTINCT * FROM soort";
+
+
+    //Planten opvragen bij opstart, indien geen ingegeven zoekcriteria er toch iets weergegeven wordt.
+    static final String GETALLPLANTSBYINITIALISE =
+            "SELECT * FROM plant ";
+
+
+    //Planten opzoeken d.m.v.  plantennaam
+   static final String GETPLANTTBYNAAM =
+            "SELECT * FROM plant WHERE fgsv LIKE ?";
+
+    //Planten opzoeken d.m.v. Volledige PlantenFamilie
+  static final String GETPLANTTBYFAMILIE =
+            "SELECT * FROM plant WHERE familie LIKE ?";
+
+    //Planten opzoeken d.m.v. Volledige plantentype
+     static final String GETPLANTTBYTYPE =
+            "SELECT * FROM plant WHERE type = ?";
+
+    //Planten opzoeken d.m.v. Volledige plantenGeslacht
+ static final String GETPLANTTBYGESLACHT =
+            "SELECT * FROM plant WHERE geslacht = ?";
+
+    //Planten opzoeken d.m.v. Volledige plantenSoort
+     static final String GETPLANTTBYSOORT =
+            "SELECT * FROM plant WHERE soort = ?";
+
+}
